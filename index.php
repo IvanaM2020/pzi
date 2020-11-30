c018c8f1296268e0627d08c458e0fe66ee1399ca
IvanaM2020 <75065653+IvanaM2020@users.noreply.github.com>
2020-11-30 16:36:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php 
+    require("nav.php");
+?>
+<html lang="en">
+
+	
+<body>
+<style>
+.fit-image{
+width: 100%;
+object-fit: cover;
+height: 300px; 
+}
+</style>
+	<div class="container">
+	<div class="row">
+    <div class="col-sm-9">
+		
+			  <div class="col-sm-3">
+    		<div class="thumbnail">
+				<img src="uploads/download.jpg" alt="slika" class="img-responsive" width = "100%" ></div>
+
+                    <p>Ime: burger</p><h4> Cijena: 1</h4>
+                    <p></p>
+
+					<p><a href="dodajudostavu.php?hrana=3" class="btn btn-primary" role="button">Dodaj</a></p>
+				</div>
+                <div class="col-sm-3">
+    		<div class="thumbnail">
+				<img src="uploads/download.jpg" alt="slika" class="img-responsive" width = "100%" ></div>
+
+                    <p>Ime: burger</p><h4> Cijena: 1</h4>
+                    <p></p>
+
+					<p><a href="dodajudostavu.php?hrana=3" class="btn btn-primary" role="button">Dodaj</a></p>
+				</div>
+                <div class="col-sm-3">
+    		<div class="thumbnail">
+				<img src="uploads/download.jpg" alt="slika" class="img-responsive" width = "100%" ></div>
+
+                    <p>Ime: burger</p><h4> Cijena: 1</h4>
+                    <p></p>
+
+					<p><a href="dodajudostavu.php?hrana=3" class="btn btn-primary" role="button">Dodaj</a></p>
+				</div>
+                <div class="col-sm-3">
+    		<div class="thumbnail">
+				<img src="uploads/download.jpg" alt="slika" class="img-responsive" width = "100%" ></div>
+
+                    <p>Ime: burger</p><h4> Cijena: 1</h4>
+                    <p></p>
+
+					<p><a href="dodajudostavu.php?hrana=3" class="btn btn-primary" role="button">Dodaj</a></p>
+				</div>
+                <div class="col-sm-3">
+    		<div class="thumbnail">
+				<img src="uploads/download.jpg" alt="slika" class="img-responsive" width = "100%" ></div>
+
+                    <p>Ime: burger</p><h4> Cijena: 1</h4>
+                    <p></p>
+
+					<p><a href="dodajudostavu.php?hrana=3" class="btn btn-primary" role="button">Dodaj</a></p>
+				</div>
+
+
+					
+
+	</div>
+
+	</div>
+
+	</div>
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+	</body></html>
---
+++ info.php
@@ -0,0 +1,26 @@
+<?php
+require("nav.php");
+?>
+<h1>O projektu</h1>
+<p>Cilj ovoga projekta je napraviti web aplikaciju za narudzbu hrane</p>
+<h1>Tehnologije</h1>
+    <ul>
+    <li>
+        PHP
+    </li>
+    <li>Bootstrap 4</li>
+    <li>MySql baza</li>
+    </ul>
+<h1>Zivotopis</h1>
+<section>
+<p>
+Zivotopis ovdje
+</p>
+</section>
+
+<h1>Motivacija</h1>
+<section>
+<p>
+    Smanjiti broj zaraženih COVID virusom tako što bi smanjili ljudski kontakt i guzve po restoranima.
+</p>
+</section>
